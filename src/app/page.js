3e6da376637f0052c415d3a87a0f2c5f6ca3c42d
David <david@innoxapp.com>
2024-01-30 13:16:54 +0100

    analytics

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import Hero from "./components/Hero/Hero";
 import Roadmap from "./components/Roadmap/Roadmap";
 import Tokenomics from "./components/Tokenomics/Tokenomics";
 import styles from "./page.module.css";
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
       <Contribution />
       <Roadmap />
       <Tokenomics />
+      <Analytics />
     </main>
   );
 }
